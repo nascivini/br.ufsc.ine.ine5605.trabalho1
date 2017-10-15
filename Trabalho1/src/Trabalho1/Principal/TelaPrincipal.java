082fb6c10927af4ece90f3e7e45463962ed28662
nascivini <31168053+nascivini@users.noreply.github.com>
2017-10-14 22:48:33 -0300

    Alterações na tela principal para permitir o acesso aos outros módulos do sistema. Testes já são possíveis! =)

--- Trabalho1/src/Trabalho1/Principal/TelaPrincipal.java
+++ Trabalho1/src/Trabalho1/Principal/TelaPrincipal.java
@@ -1,5 +1,6 @@
 package Trabalho1.Principal;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -22,17 +23,42 @@ public class TelaPrincipal {
     }
     
     public void inicia(){
+        try{
+
+            System.out.println("--- Bem vindo ao sistema! ---");
+            System.out.println("--- Para acessar os módulos, escolha uma das opções abaixo e tecle enter. ---");
+            System.out.println("--- 1 - Cadastro de Cargos ---");
+            System.out.println("--- 2 - Cadastro de Funcionarios ---");
+            System.out.println("--- 3 - Acessar Setor Financeiro  ---");
         
-        System.out.println("--- Bem vindo ao sistema! ---");
-        System.out.println("--- Para acessar os módulos, escolha uma das opções abaixo e tecle enter. ---");
-        System.out.println("--- 1 - Cadastro de Cargos ---");
-        System.out.println("--- 2 - Cadastro de Funcionarios ---");
-        System.out.println("--- 3 - Acessar Setor Financeiro  ---");
-        
-        int opcao = teclado.nextInt();
-        
-        if(opcao == 1){
-                this.controladorPrincipal.getControladorCargo().getTelaCargo().inicia();
+            int opcao = teclado.nextInt();
+            switch(opcao){
+                case(1):
+                    this.getControladorPrincipal().getControladorCargo().getTelaCargo().inicia();
+                    break;
+                
+                case(2):
+                    this.getControladorPrincipal().getControladorFuncionario().getTelaFuncionario().inicia();
+                    break;
+                
+                case(3):
+                    this.getControladorPrincipal().getControladorAcesso().getTelaAcesso().inicia();
+                    break;
+            
+                default:    
+                    throw new IllegalArgumentException("Opção inválida! Escolha uma opção dentre as opções da lista.");
+            }
         }
-    }
+        catch(IllegalArgumentException e){
+            e.getMessage();
+        }
+        
+        catch(InputMismatchException e){
+            System.out.println("Opção inválida! Escolha uma opção dentre as opções da lista.");
+            e.getMessage();
+        }
+        
+    }   
+
 }
+
