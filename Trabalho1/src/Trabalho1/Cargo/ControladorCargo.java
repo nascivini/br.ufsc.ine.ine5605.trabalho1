16cf09ca0fc7b9b59c7bed63e520b4627230e9c5
nascivini <31168053+nascivini@users.noreply.github.com>
2017-10-16 10:44:38 -0200

    Efetuada inclusão da verificação de horários no controlador

--- Trabalho1/src/Trabalho1/Cargo/ControladorCargo.java
+++ Trabalho1/src/Trabalho1/Cargo/ControladorCargo.java
@@ -4,6 +4,7 @@ import Trabalho1.Principal.ControladorPrincipal;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -13,12 +14,11 @@ import java.util.Date;
  * @author Marco Aurelio Geremias
  */
 public class ControladorCargo implements IControladorCargo {
-    
+
     private final ControladorPrincipal controladorPrincipal;
     private final ArrayList<Cargo> cargos;
     private final TelaCargo telaCargo;
     private int sequencialCargo;
-    
 
     public ControladorCargo(ControladorPrincipal controladorPrincipal) {
         this.cargos = new ArrayList<>();
@@ -26,7 +26,7 @@ public class ControladorCargo implements IControladorCargo {
         this.controladorPrincipal = controladorPrincipal;
         this.sequencialCargo = 0;
     }
-    
+
     public ArrayList<Cargo> getCargos() {
         return cargos;
     }
@@ -34,18 +34,18 @@ public class ControladorCargo implements IControladorCargo {
     public TelaCargo getTelaCargo() {
         return telaCargo;
     }
-    
+
     public ControladorPrincipal getControladorPrincipal() {
         return this.controladorPrincipal;
     }
-    
+
     @Override
-    public Cargo incluirCargo(DadosCargo conteudo, int codigo){
+    public Cargo incluirCargo(DadosCargo conteudo, int codigo) {
         Cargo novo = new Cargo(conteudo, codigo);
         cargos.add(novo);
-        return novo; 
+        return novo;
     }
-    
+
     @Override
     public boolean excluirCargo(int codigo) {
         if (this.findCargoByCodigo(codigo) != null) {
@@ -58,7 +58,7 @@ public class ControladorCargo implements IControladorCargo {
         }
         return false;
     }
-    
+
     @Override
     public Cargo alterarCargo(DadosCargo conteudo, int codigo) {
         if (conteudo != null) {
@@ -68,68 +68,138 @@ public class ControladorCargo implements IControladorCargo {
                     cargoLista.setPermiteAcesso(conteudo.permiteAcesso);
                     cargoLista.setNome(conteudo.nome);
                     //cargoLista.setHorario(conteudo.horarios);
-                    
+
                     return cargoLista;
-                } 
+                }
             }
         }
         return null;
     }
-    
+
     @Override
-    public Cargo findCargoByCodigo(int codigo){
-       if(codigo > 0){ 
-           for(Cargo cargoLista : this.cargos){
-               if(codigo == cargoLista.getCodigo()){
-                   return cargoLista;
-               }
-           }
-       }
-       return null;
+    public Cargo findCargoByCodigo(int codigo) {
+        if (codigo > 0) {
+            for (Cargo cargoLista : this.cargos) {
+                if (codigo == cargoLista.getCodigo()) {
+                    return cargoLista;
+                }
+            }
+        }
+        return null;
     }
-    
+
     @Override
-    public boolean findCargoByNome(String nome){
-        for(Cargo cargoAtual : cargos){
-            if(cargoAtual.getNome().equals(nome)){
+    public boolean findCargoByNome(String nome) {
+        for (Cargo cargoAtual : cargos) {
+            if (cargoAtual.getNome().equals(nome)) {
                 return true;
             }
         }
         return false;
     }
-    
+
     /**
-     * Gera um número sequencial para uso da TelaCargo no cadastro de um novo cargo, permitindo a consistência dos códigos de cargos novos a serem cadastrados no sistema.
+     * Gera um número sequencial para uso da TelaCargo no cadastro de um novo
+     * cargo, permitindo a consistência dos códigos de cargos novos a serem
+     * cadastrados no sistema.
+     *
      * @return Sequencial gerado.
      */
-    public int geraSequencialCargo(){
+    public int geraSequencialCargo() {
         this.sequencialCargo++;
         return this.sequencialCargo;
     }
-    
+
     /**
      * É usado pela TelaCargo para listar os cargos já cadastrados.
      */
     //ficamos em dúvida entre colocar este método na tela ou no controlador
-    public void listarCargos(){
+    public void listarCargos() {
         DateFormat formatador = new SimpleDateFormat("HH:mm");
-        for(Cargo cargoLista : cargos){
+        for (Cargo cargoLista : cargos) {
             System.out.println("Nome: " + cargoLista.getNome() + " | Código: " + cargoLista.getCodigo());
             System.out.println("Horários deste cargo :");
-            for(int i = 0; i < cargoLista.getHorarios().size(); i = i+2){
+            for (int i = 0; i < cargoLista.getHorarios().size(); i = i + 2) {
                 Date horario1 = cargoLista.getHorarios().get(i).getTime();
-                Date horario2 = cargoLista.getHorarios().get(i+1).getTime();
+                Date horario2 = cargoLista.getHorarios().get(i + 1).getTime();
                 System.out.println("De : " + formatador.format(horario1) + " à " + formatador.format(horario2) + ";");
             }
             System.out.println();
         }
     }
-    
+
     /**
      * Reduz o sequencial de cargos em uma unidade.
      */
     public void reduzSequencialCargo() {
-       this.sequencialCargo = sequencialCargo - 1;
+        this.sequencialCargo = sequencialCargo - 1;
     }
 
+    public boolean verificaHorarios(ArrayList<Calendar> horarios, Calendar horario1, Calendar horario2) {
+        if (horarios.isEmpty()) {
+            if (horario1.getTime() != horario2.getTime()) {
+                return true;
+            }
+        } else {
+            Calendar menor = horarios.get(0);
+            for (Calendar horarioAtual : horarios) {
+                Calendar horario = horarioAtual;
+                if (horario.getTime().before(menor.getTime())) {
+                    menor = horario;
+                }
+            }
+
+            boolean horario1OK = false;
+            boolean horario2OK = false;
+
+            for (int i = 1; i < horarios.size(); i = i + 2) {
+                if (horarios.get(i - 1).after(horarios.get(i))) {
+                    if (horario1.after(horarios.get(i)) && horario1.before(horarios.get(i - 1))) {
+                        horario1OK = true;
+                    } else {
+                        horario1OK = false;
+                        throw new IllegalArgumentException("Horário inicial está dentro de uma faixa de horários já cadastrada ou é nulo. Verifique o mesmo e tente novamente. O cargo não foi cadastrado.");
+                    }
+                } else {
+                    Calendar ultimoMin = Calendar.getInstance();
+                    ultimoMin.set(0, 0, 0, 23, 59);
+                    if ((horario1.after(horarios.get(i)) && horario1.before(ultimoMin)) || horario1.before(menor)) {
+                        horario1OK = true;
+                    } else {
+                        horario1OK = false;
+                        throw new IllegalArgumentException("Horário inicial está dentro de uma faixa de horários já cadastrada ou é nulo. Verifique o mesmo e tente novamente. O cargo não foi cadastrado.");
+                    }
+                }
+            }
+            for (int i = 0; i < horarios.size(); i = i + 2) {
+                if (horarios.get(i).after(horarios.get(i + 1))) {
+                    if ((horario2.after(horario1)) && (horario2.before(menor))) {
+                        horario2OK = true;
+                    } else {
+                        horario2OK = false;
+                        throw new IllegalArgumentException("Horário inicial está dentro de uma faixa de horários já cadastrada ou é nulo. Verifique o mesmo e tente novamente. O cargo não foi cadastrado.");
+                    }
+                } else {
+                    Calendar ultimoMin = Calendar.getInstance();
+                    ultimoMin.set(0, 0, 0, 23, 59);
+                    if (!(horario2.after(horario2) && horario2.before(ultimoMin)) || horario2.before(horarios.get(0))) {
+                        horario2OK = true;
+                    } else {
+                        horario2OK = false;
+                        throw new IllegalArgumentException("Horário inicial está dentro de uma faixa de horários já cadastrada ou é nulo. Verifique o mesmo e tente novamente. O cargo não foi cadastrado.");
+                    }
+                }
+            }
+            if(horario1OK && (!horario2OK)){
+                return false;
+            }
+            else if((!horario1OK) && horario2OK){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
 }
